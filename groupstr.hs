c7365606051c48ab2b6d84f27d304d7b7dd3b0e8
prasad iyer <p.c.iyer@gmail.com>
2019-06-06 15:06:57 -0700

    Create groupstr.hs

---
+++ groupstr.hs
@@ -0,0 +1,17 @@
+import Data.List (group)
+{-
+ - gab $ gab0 ['a', 'a', 'a', 'b', 'c', 'c','c','c','d','e','e']
+"a3bc4de2"
+-}
+
+
+gab0 :: [Char] -> [(Char, Int)]
+gab0 = map(\l@(x:xs) -> (x, length l)) . group
+
+gab1 :: (Char, Int) -> String
+gab1 (d, 1) = [d]
+gab1 (d, x) = [d] ++ show x
+
+
+gab :: [(Char, Int)] -> String
+gab = foldr(\x acc ->  gab1 x ++ acc) []   
