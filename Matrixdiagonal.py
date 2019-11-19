52c22fc178c1dc8dc3c42770b6062ba51d37387b
prasad iyer <p.c.iyer@gmail.com>
2019-11-19 12:11:31 -0800

    Create Matrixdiagonal.py

---
+++ Matrixdiagonal.py
@@ -0,0 +1,14 @@
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+y = 0;
+pox = 0
+
+g = 0
+for i in range(0,6):
+  print ("---")
+  for k in range(0,10):
+    poy = i - k
+    if (  poy < 0  or k > len(matrix) -1 or poy > len(matrix) -1 ):
+      continue
+    # print (str(k) + " " + str(poy))
+    print (matrix[k][poy])
+
