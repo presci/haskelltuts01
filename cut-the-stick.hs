3333b96032ecfd3b5d851b5467ffd9cb9101452f
prasad iyer <p.c.iyer@gmail.com>
2019-12-11 10:27:14 -0800

    Update cut-the-stick.hs

--- cut-the-stick.hs
+++ cut-the-stick.hs
@@ -2,10 +2,18 @@ import Data.List
 import Data.Ord
 
 solve::[Int] -> [Int]
-solve xss = solve' xss  
-    where 
-    solve' [] = []
-    solve' xss = 
-        let m = minimum xss
-            g =  foldl (\acc x -> if (x - m)  > 0 then (x - m):acc else acc) [] xss
-            in length xss:solve' g 
+solve xss = solve' xss
+  where
+  solve'::[Int]->[Int]
+  solve' [] = []
+  solve' xss =
+    let m = minimum xss
+        g =  foldl (\acc x -> if (x - m)  > 0 then (x - m):acc else acc) [] xss
+          in length xss:solve' g
+
+main::IO()
+main = do
+  k <- readLn ::IO Int
+  g <- getLine
+  let pro = map(read::String-> Int) . words $ g
+  putStr $ intercalate "\n" $ map show $ solve pro
