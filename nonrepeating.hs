5c0f1f76c21d5cde76b98c3a5cceb7c1fef82734
prasad iyer <p.c.iyer@gmail.com>
2020-02-21 10:37:03 -0800

    updated with norepeat2

--- nonrepeating.hs
+++ nonrepeating.hs
@@ -1,4 +1,5 @@
 import Data.Maybe
+import qualified Data.Map.Strict as Map
 
 norepeat::[Char]-> Maybe Char
 norepeat chs = norepeat' chs
@@ -15,3 +16,18 @@ norepeat chs = norepeat' chs
         True -> drop' k xss
         False -> norepeat' (x:xss)
         
+
+
+norepeat2::[Char] -> Maybe Char
+norepeat2 xss = getnorepeat xss ( getMappedChars xss Map.empty)
+    where
+        getMappedChars::[Char] -> Map.Map Char Integer -> Map.Map Char Integer
+        getMappedChars [] g = g
+        getMappedChars (x:xss) g = case Map.lookup x g of
+            Nothing -> getMappedChars xss (Map.insert x 1 g)
+            Just a -> getMappedChars xss (Map.insertWith (+) x 1 g)
+        getnorepeat::[Char] -> Map.Map Char Integer -> Maybe Char
+        getnorepeat [] _ = Nothing
+        getnorepeat (x:xss) g = case Map.findWithDefault 2 x g == 1 of
+            True -> Just x
+            False -> getnorepeat xss g 
